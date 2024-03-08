22a29bfb63bfb7d0a58955c41e28a2f67db67609
MrRamsey <ramseyd@clevelandcc.edu>
2024-03-08 09:44:33 -0500

    Hello World Demo

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    /*
+     * This is a block comment.
+     * It can have as many lines as we want.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello \nWorld!"); // \n = new line
+        System.out.println("Another\" Hello!"); // \" treats " like a character
+        System.out.print("hello \\");// \\ treats \ like a character
+        System.out.print("\tworld!");// \t inserts a tab character
+    }
+
+}
